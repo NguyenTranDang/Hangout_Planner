31ce166939af0c35c6e567afb9ee65890f37cc87
NguyenTranDang <155166638+NguyenTranDang@users.noreply.github.com>
2024-09-27 13:13:48 +0000

    update image

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+from flask import Flask, request, render_template
 
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    image_url = "?.jpg"
+    return render_template("index.html", image_url = image_url)
+
+@app.route("/input_page")
+def input_page():
+    return render_template("input_page.html")
+
+if __name__ == "__main__":
+    app.run()
+    
